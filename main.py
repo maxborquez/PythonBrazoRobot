070591563ae691e5f4da0302fc95fd04cd052158
maximiliano <max.borquezc@gmail.com>
2024-05-24 13:26:10 -0400

    fix

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import serial
 
 class ControlBrazoRobot:
     def __init__(self, ventana):
+        self.azul = "#051333"
+        self.rojo = "#932727"
+        self.verde = "#1cb360"
         self.ventana = ventana
         self.ventana.title("Control brazo robotico")
         self.ventana.configure(bg='#051333')
@@ -22,16 +25,17 @@ class ControlBrazoRobot:
         self.seccion_2()
         self.seccion_servos()
         self.seccion_panel()
+        self.seccion_5()
         self.seccion_6()
 
     def seccion_conexion(self):
-        label_conexion = tk.Label(self.ventana, text="Conexión", bg='#051333', fg='white', font=self.text_font)
+        label_conexion = tk.Label(self.ventana, text="Conexión", bg=self.azul, fg='white', font=self.text_font)
         label_conexion.grid(row=0, column=0, padx=10, pady=5, sticky="nw")
 
-        frame_conexion = tk.Frame(self.ventana, bg='#051333')
+        frame_conexion = tk.Frame(self.ventana, bg=self.azul)
         frame_conexion.grid(row=1, column=0, padx=10, pady=5, sticky="nw")
 
-        self.label_estado = tk.Label(frame_conexion, text="Desconectado", font=self.text_font, bg="#932727", fg="white", width=20)
+        self.label_estado = tk.Label(frame_conexion, text="Desconectado", font=self.text_font, bg=self.rojo, fg="white", width=20)
         self.label_estado.grid(row=1, column=0, padx=5, pady=5)
 
         self.combo_com = ttk.Combobox(frame_conexion, values=["COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8"])
@@ -45,28 +49,42 @@ class ControlBrazoRobot:
         btn_cerrar.grid(row=4, column=0, padx=5, pady=5)
 
     def seccion_2(self):
-        label_cinematica_directa = tk.Label(self.ventana, text="Seccion 2", bg='#051333', fg='white', font=self.text_font)
-        label_cinematica_directa.grid(row=0, column=1, padx=10, pady=5, sticky="nw")
+        label_seccion_2 = tk.Label(self.ventana, text="Seccion 2", bg=self.azul, fg='white', font=self.text_font)
+        label_seccion_2.grid(row=0, column=1, padx=10, pady=5, sticky="nw")
 
-        frame_cinematica_directa = tk.Frame(self.ventana, bg='#051333')
-        frame_cinematica_directa.grid(row=1, column=1, padx=10, pady=5, sticky="nw")
+        label_seccion_2 = tk.Frame(self.ventana, bg=self.azul)
+        label_seccion_2.grid(row=1, column=1, padx=10, pady=5, sticky="nw")
 
     def seccion_servos(self):
-        label_entradas = tk.Label(self.ventana, text="Servomotores", bg='#051333', fg='white', font=self.text_font)
-        label_entradas.grid(row=0, column=2, padx=10, pady=5, sticky="nw")
+        label_servos = tk.Label(self.ventana, text="Servomotores", bg=self.azul, fg='white', font=self.text_font)
+        label_servos.grid(row=0, column=2, padx=10, pady=5, sticky="nw")
 
-        frame_entradas = tk.Frame(self.ventana, bg='#051333')
-        frame_entradas.grid(row=1, column=2, padx=10, pady=5, sticky="nw")
+        label_servos = tk.Frame(self.ventana, bg=self.azul)
+        label_servos.grid(row=1, column=2, padx=10, pady=5, sticky="nw")
 
-        for i in range(6):
-            tk.Label(frame_entradas, text=f"Ángulos Servomotor {i + 1}", bg='#051333', fg='white', font=self.text_font).grid(row=i, column=0, padx=5, pady=5)
-            tk.Spinbox(frame_entradas, from_=0, to=180).grid(row=i, column=1, padx=5, pady=5)
+        tk.Label(label_servos, text="Ángulos Servomotor 1", bg=self.azul, fg='white', font=self.text_font).grid(row=0, column=0, padx=5, pady=5)
+        tk.Spinbox(label_servos, from_=0, to=180).grid(row=0, column=1, padx=5, pady=5)
+
+        tk.Label(label_servos, text="Ángulos Servomotor 2", bg=self.azul, fg='white', font=self.text_font).grid(row=1, column=0, padx=5, pady=5)
+        tk.Spinbox(label_servos, from_=0, to=180).grid(row=1, column=1, padx=5, pady=5)
+
+        tk.Label(label_servos, text="Ángulos Servomotor 3", bg=self.azul, fg='white', font=self.text_font).grid(row=2, column=0, padx=5, pady=5)
+        tk.Spinbox(label_servos, from_=0, to=180).grid(row=2, column=1, padx=5, pady=5)
+
+        tk.Label(label_servos, text="Ángulos Servomotor 4", bg=self.azul, fg='white', font=self.text_font).grid(row=3, column=0, padx=5, pady=5)
+        tk.Spinbox(label_servos, from_=0, to=180).grid(row=3, column=1, padx=5, pady=5)
+
+        tk.Label(label_servos, text="Ángulos Servomotor 5", bg=self.azul, fg='white', font=self.text_font).grid(row=4, column=0, padx=5, pady=5)
+        tk.Spinbox(label_servos, from_=0, to=180).grid(row=4, column=1, padx=5, pady=5)
+
+        tk.Label(label_servos, text="Ángulos Servomotor 6", bg=self.azul, fg='white', font=self.text_font).grid(row=5, column=0, padx=5, pady=5)
+        tk.Spinbox(label_servos, from_=0, to=180).grid(row=5, column=1, padx=5, pady=5)
 
     def seccion_panel(self):
-        label_panel = tk.Label(self.ventana, text="Panel", bg='#051333', fg='white', font=self.text_font)
+        label_panel = tk.Label(self.ventana, text="Panel", bg=self.azul, fg='white', font=self.text_font)
         label_panel.grid(row=2, column=0, padx=10, pady=5, sticky="nw")
 
-        frame_panel = tk.Frame(self.ventana, bg='#051333')
+        frame_panel = tk.Frame(self.ventana, bg=self.azul)
         frame_panel.grid(row=3, column=0, padx=10, pady=5, sticky="nw")
 
         buttons = ["Registro 1", "Registro 2", "Registro 3", "Registro 4", "Registro 5", "Home", "Ejecutar", "Limpiar"]
@@ -75,11 +93,18 @@ class ControlBrazoRobot:
             column = 0 if i < 5 else 1
             tk.Button(frame_panel, text=text, font=self.button_font).grid(row=row, column=column, padx=5, pady=5)
 
+    def seccion_5(self):
+        label_seccion_5 = tk.Label(self.ventana, text="Seccion 5", bg=self.azul, fg='white', font=self.text_font)
+        label_seccion_5.grid(row=2, column=1, padx=10, pady=5, sticky="nw")
+
+        label_seccion_5 = tk.Frame(self.ventana, bg=self.azul)
+        label_seccion_5.grid(row=3, column=1, padx=10, pady=5, sticky="nw")
+
     def seccion_6(self):
-        label_graficos = tk.Label(self.ventana, text="Seccion 6", bg='#051333', fg='white', font=self.text_font)
+        label_graficos = tk.Label(self.ventana, text="Seccion 6", bg=self.azul, fg='white', font=self.text_font)
         label_graficos.grid(row=2, column=2, padx=10, pady=5, sticky="nw")
 
-        frame_graficos = tk.Frame(self.ventana, bg='#051333')
+        frame_graficos = tk.Frame(self.ventana, bg=self.azul)
         frame_graficos.grid(row=3, column=2, padx=10, pady=5, sticky="nw")
 
         try:
@@ -105,7 +130,7 @@ class ControlBrazoRobot:
                 self.SerialPort1.stopbits = serial.STOPBITS_ONE
                 self.SerialPort1.port = self.combo_com.get()
                 self.SerialPort1.open()
-                self.label_estado.config(text="Conectado", bg="#1cb360")
+                self.label_estado.config(text="Conectado", bg=self.verde)
                 messagebox.showinfo(message="Puerto Conectado")
             except Exception as e:
                 messagebox.showerror(message=f"Error al conectar: {e}")
@@ -115,7 +140,7 @@ class ControlBrazoRobot:
     def click_desconectar(self):
         if self.SerialPort1.isOpen():
             self.SerialPort1.close()
-            self.label_estado.config(text="Desconectado", bg="#932727")
+            self.label_estado.config(text="Desconectado", bg=self.rojo)
             messagebox.showinfo(message="Puerto Desconectado")
         else:
             messagebox.showinfo(message="El puerto ya está desconectado")
