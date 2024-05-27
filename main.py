517b0113cd3a7f87157f9b8f4f6e777157582e2b
maximiliano <max.borquezc@gmail.com>
2024-05-27 11:24:35 -0400

    new main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import ttk, messagebox
 import tkinter.font as tkfont
 from PIL import Image, ImageTk
 import serial
+import csv
 
 class ControlBrazoRobot:
     def __init__(self, ventana):
@@ -11,13 +12,14 @@ class ControlBrazoRobot:
         self.verde = "#1cb360"
         self.ventana = ventana
         self.ventana.title("Control brazo robotico")
-        self.ventana.configure(bg='#051333')
-        self.ventana.geometry("800x600")
+        self.ventana.configure(bg=self.azul)
+        self.ventana.geometry("1150x600")
 
         self.button_font = tkfont.Font(family="Arial", size=10, weight="bold")
         self.text_font = tkfont.Font(family="Arial", size=12, weight="bold")
 
         self.SerialPort1 = serial.Serial()
+        self.registros = self.leer_registros_csv("registros.csv")
         self.create_widgets()
 
     def create_widgets(self):
@@ -27,6 +29,19 @@ class ControlBrazoRobot:
         self.seccion_panel()
         self.seccion_5()
         self.seccion_6()
+
+    def leer_registros_csv(self, filename):
+        registros = {}
+        with open(filename, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            current_r = None
+            for row in reader:
+                if len(row) > 0 and row[0].startswith('r'):
+                    current_r = row[0]
+                    registros[current_r] = []
+                elif current_r:
+                    registros[current_r].append(row)
+        return registros
 
     def seccion_conexion(self):
         label_conexion = tk.Label(self.ventana, text="Conexión", bg=self.azul, fg='white', font=self.text_font)
@@ -49,7 +64,7 @@ class ControlBrazoRobot:
         btn_cerrar.grid(row=4, column=0, padx=5, pady=5)
 
     def seccion_2(self):
-        label_seccion_2 = tk.Label(self.ventana, text="Seccion 2", bg=self.azul, fg='white', font=self.text_font)
+        label_seccion_2 = tk.Label(self.ventana, text="Sección 2", bg=self.azul, fg='white', font=self.text_font)
         label_seccion_2.grid(row=0, column=1, padx=10, pady=5, sticky="nw")
 
         frame_seccion_2 = tk.Frame(self.ventana, bg=self.azul)
@@ -64,7 +79,7 @@ class ControlBrazoRobot:
 
         self.spinbox_servos = []
         for i in range(6):
-            tk.Label(frame_servos, text=f"Posicion servo {i+1}", bg=self.azul, fg='white', font=self.text_font).grid(row=i, column=0, padx=5, pady=5)
+            tk.Label(frame_servos, text=f"Posición servo {i+1}", bg=self.azul, fg='white', font=self.text_font).grid(row=i, column=0, padx=5, pady=5)
             spinbox = tk.Spinbox(frame_servos, from_=0, to=180, width=5, font=15)
             spinbox.grid(row=i, column=1, padx=5, pady=5)
             self.spinbox_servos.append(spinbox)
@@ -79,19 +94,19 @@ class ControlBrazoRobot:
         frame_panel = tk.Frame(self.ventana, bg=self.azul)
         frame_panel.grid(row=3, column=0, padx=10, pady=5, sticky="nw")
 
-        btn_registro1 = tk.Button(frame_panel, text="Registro 1", font=self.button_font)
+        btn_registro1 = tk.Button(frame_panel, text="Registro 1", font=self.button_font, command=lambda: self.mostrar_registro('r1'))
         btn_registro1.grid(row=1, column=0, padx=5, pady=5)
 
-        btn_registro2 = tk.Button(frame_panel, text="Registro 2", font=self.button_font)
+        btn_registro2 = tk.Button(frame_panel, text="Registro 2", font=self.button_font, command=lambda: self.mostrar_registro('r2'))
         btn_registro2.grid(row=2, column=0, padx=5, pady=5)
 
-        btn_registro3 = tk.Button(frame_panel, text="Registro 3", font=self.button_font)
+        btn_registro3 = tk.Button(frame_panel, text="Registro 3", font=self.button_font, command=lambda: self.mostrar_registro('r3'))
         btn_registro3.grid(row=3, column=0, padx=5, pady=5)
 
-        btn_registro4 = tk.Button(frame_panel, text="Registro 4", font=self.button_font)
+        btn_registro4 = tk.Button(frame_panel, text="Registro 4", font=self.button_font, command=lambda: self.mostrar_registro('r4'))
         btn_registro4.grid(row=4, column=0, padx=5, pady=5)
 
-        btn_registro5 = tk.Button(frame_panel, text="Registro 5", font=self.button_font)
+        btn_registro5 = tk.Button(frame_panel, text="Registro 5", font=self.button_font, command=lambda: self.mostrar_registro('r5'))
         btn_registro5.grid(row=5, column=0, padx=5, pady=5)
 
         btn_home = tk.Button(frame_panel, text="  Home  ", font=self.button_font)
@@ -110,14 +125,25 @@ class ControlBrazoRobot:
         btn_quitar.grid(row=5, column=1, padx=5, pady=5)
 
     def seccion_5(self):
-        label_seccion_5 = tk.Label(self.ventana, text="Seccion 5", bg=self.azul, fg='white', font=self.text_font)
+        label_seccion_5 = tk.Label(self.ventana, text="Sección 5", bg=self.azul, fg='white', font=self.text_font)
         label_seccion_5.grid(row=2, column=1, padx=10, pady=5, sticky="nw")
 
-        frame_seccion_5 = tk.Frame(self.ventana, bg=self.azul)
-        frame_seccion_5.grid(row=3, column=1, padx=10, pady=5, sticky="nw")
+        self.frame_seccion_5 = tk.Frame(self.ventana, bg=self.azul)
+        self.frame_seccion_5.grid(row=3, column=1, padx=10, pady=5, sticky="nw")
+
+        for widget in self.frame_seccion_5.winfo_children():
+            widget.destroy()
+
+        self.tree = ttk.Treeview(self.frame_seccion_5, columns=[f"#{i}" for i in range(1, 7)], show='headings')
+
+        for i in range(1, 7):
+            self.tree.heading(f"#{i}", text=f"Col {i}")
+            self.tree.column(f"#{i}", width=100)
+
+        self.tree.pack(expand=True, fill='both')
 
     def seccion_6(self):
-        label_seccion_6 = tk.Label(self.ventana, text="Seccion 6", bg=self.azul, fg='white', font=self.text_font)
+        label_seccion_6 = tk.Label(self.ventana, text="Sección 6", bg=self.azul, fg='white', font=self.text_font)
         label_seccion_6.grid(row=2, column=2, padx=10, pady=5, sticky="nw")
 
         frame_seccion_6 = tk.Frame(self.ventana, bg=self.azul)
@@ -170,12 +196,29 @@ class ControlBrazoRobot:
         else:
             messagebox.showwarning(message="El puerto no está conectado")
 
+    def mostrar_registro(self, registro):
+        for widget in self.frame_seccion_5.winfo_children():
+            widget.destroy()
+
+        self.tree = ttk.Treeview(self.frame_seccion_5, columns=[f"#{i}" for i in range(1, 7)], show='headings')
+
+        for i in range(1, 7):
+            self.tree.heading(f"#{i}", text=f"Servo {i}")
+            self.tree.column(f"#{i}", width=100)
+
+        self.tree.pack(expand=True, fill='both')
+
+        if registro in self.registros:
+            if len(self.registros[registro]) == 0:
+                tk.Label(self.frame_seccion_5, text="No hay datos", bg=self.azul, fg='white', font=self.text_font).pack(padx=5, pady=5)
+            else:
+                for row in self.registros[registro]:
+                    self.tree.insert("", "end", values=row)
 
 def main():
     ventana = tk.Tk()
     app = ControlBrazoRobot(ventana)
     ventana.mainloop()
-
 
 if __name__ == "__main__":
     main()
